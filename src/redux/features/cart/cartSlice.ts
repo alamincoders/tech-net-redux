44cefa7f6c2bca98acf44713396a56eb408eced3
alamincoders <developeralamin03@gmail.com>
2023-07-07 20:00:00 -0600

    module 21 video 1 =>>>>>>>>> store & hook setup

--- src/redux/features/cart/cartSlice.ts
+++ src/redux/features/cart/cartSlice.ts
@@ -1,5 +1,5 @@
-import { createSlice } from '@reduxjs/toolkit';
 import { IProduct } from '@/types/globalTypes';
+import { createSlice } from '@reduxjs/toolkit';
 
 interface ICart {
   products: IProduct[];
